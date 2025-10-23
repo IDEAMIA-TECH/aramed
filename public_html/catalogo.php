da2a7c84fd208ef6fd954a74a4c822678d604fac
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-10-22 21:05:21 -0600

    Update catalogo.php

--- public_html/catalogo.php
+++ public_html/catalogo.php
@@ -405,6 +405,11 @@ function buildFilterUrl($params = []) {
 <body class="catalogo-page">
     
     <!-- ========================================
+         TOPBAR
+         ======================================== -->
+    <?php include INCLUDES_PATH . '/topbar.php'; ?>
+    
+    <!-- ========================================
          NAVBAR
          ======================================== -->
     <?php include INCLUDES_PATH . '/navbar.php'; ?>
